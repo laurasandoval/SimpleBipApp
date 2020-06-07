66dfd82c46875f4a5c4703a32260abdd985f5dfe
Laura <45631530+laurasandoval@users.noreply.github.com>
2020-06-07 12:45:09 -0400

    feat: basic error handling

--- main.js
+++ main.js
@@ -1,36 +1,56 @@
 let cardNumberInput = document.getElementById("card-number-input");
 let getBalanceButton = document.getElementById("get-balance");
 
-async function getBalance() {
-  if (someCard) {
-    let loadingIndicator = document.querySelector(".loader");
-    let cardNumber = cardNumberInput.value;
-    
-    loadingIndicator.classList.add("loading");
-    
-    let response = await fetch(
-      `https://api.xor.cl/bip/new.php?n=${cardNumber}`
-    );
-    let data = await response.json();
+function loading(bool) {
+  let loadingIndicator = document.querySelector(".loader");
+  bool === true
+    ? loadingIndicator.classList.add("loading")
+    : loadingIndicator.classList.remove("loading");
+}
 
-    var rawBalance = data.saldo.toString();
-    var formattedBalance = `$${rawBalance.replace(
-      /(\d)(?=(\d\d\d)+(?!\d))/g,
-      "$1."
-    )}`;
+function showError() {
+  let errorEmojis = ["💔", "😭", "😥", "🥺", "😣"];
+  let randomEmoji = errorEmojis[Math.floor(Math.random() * errorEmojis.length)];
+  let resultParentElement = document.querySelector(".results");
+  let visibleResultElement = document.querySelector(".result-value .visible");
+  let resultsTitle = document.querySelector(".results .result-title");
 
-    loadingIndicator.classList.remove("loading");
-    updateBalance(rawBalance, formattedBalance);
-  }
+  resultParentElement.setAttribute("data-has-results", "true");
+  resultParentElement.setAttribute("aria-hidden", "false");
+  resultParentElement.setAttribute("aria-busy", "false");
+  resultsTitle.innerText = "Error";
+
+  visibleResultElement.innerText = randomEmoji;
+  loading(false);
+}
+
+function getBalance() {
+  loading(true);
+  let cardNumber = cardNumberInput.value;
+
+  fetch(`https://api.xor.cl/bip/new.php?n=${cardNumber}`)
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.valida === true) {
+        let rawBalance = response.saldo.toString();
+        let formattedBalance = `$${rawBalance.replace(
+          /(\d)(?=(\d\d\d)+(?!\d))/g,
+          "$1."
+        )}`;
+
+        loading(false);
+        updateBalance(rawBalance, formattedBalance);
+      } else {
+        showError();
+      }
+    });
 }
 
 function updateBalance(rawBalance, formattedBalance) {
   let resultParentElement = document.querySelector(".results");
-  let visibleResultElement = document.querySelector(
-    ".results .result-value .visible"
-  );
+  let visibleResultElement = document.querySelector(".result-value .visible");
   let accessibleResultElement = document.querySelector(
-    ".results .result-value .accessible"
+    ".result-value .accessible"
   );
 
   resultParentElement.setAttribute("data-has-results", "true");
