916b752f4b5e6c299d9b80c29c02d21611cea659
Laura <45631530+laurasandoval@users.noreply.github.com>
2020-06-07 11:32:52 -0400

    feat: add javascript logic for retrieving balance

---
+++ main.js
@@ -0,0 +1,53 @@
+let cardNumberInput = document.getElementById("card-number-input");
+let getBalanceButton = document.getElementById("get-balance");
+
+async function getBalance() {
+  if (someCard) {
+    let cardNumber = cardNumberInput.value;
+    let response = await fetch(
+      `https://api.xor.cl/bip/new.php?n=${cardNumber}`
+    );
+    let data = await response.json();
+
+    var rawBalance = data.saldo.toString();
+    var formattedBalance = `$${rawBalance.replace(
+      /(\d)(?=(\d\d\d)+(?!\d))/g,
+      "$1."
+    )}`;
+
+    updateBalance(rawBalance, formattedBalance);
+  }
+}
+
+function updateBalance(rawBalance, formattedBalance) {
+  let resultParentElement = document.querySelector(".results");
+  let visibleResultElement = document.querySelector(
+    ".results .result-value .visible"
+  );
+  let accessibleResultElement = document.querySelector(
+    ".results .result-value .accessible"
+  );
+
+  resultParentElement.setAttribute("data-has-results", "true");
+  resultParentElement.setAttribute("aria-hidden", "false");
+  resultParentElement.setAttribute("aria-busy", "false");
+  visibleResultElement.innerText = formattedBalance;
+  accessibleResultElement.innerText = rawBalance;
+}
+
+function someCard() {
+  cardNumberInput.value.length > 0
+    ? getBalanceButton.setAttribute("disabled", "false")
+    : getBalanceButton.setAttribute("disabled", "true");
+  return cardNumberInput.value.length > 0;
+}
+
+cardNumberInput.addEventListener("input", someCard);
+getBalanceButton.addEventListener("click", getBalance);
+cardNumberInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && someCard) {
+    getBalance();
+  }
+});
+
+someCard();
